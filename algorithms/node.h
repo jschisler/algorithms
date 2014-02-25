458c93da8d00cac9aa4ca4f11e281898652f863f
John Schisler <john_schisler@hotmail.com>
2014-02-25 10:22:20 -0500

    Added circular_insert()

--- algorithms/node.h
+++ algorithms/node.h
@@ -1,4 +1,6 @@
-﻿//  This implementation is just like a stack
+﻿#include <algorithm>
+
+//  This implementation is just like a stack
 struct node {
 	node* next;
 	int data;
@@ -69,3 +71,24 @@ node* remove(node* head, node* target) {
 
 	return head;
 }
+
+node* circular_insert(node* head, int data) {
+	auto insertion = new node(data); 
+	
+	if (nullptr == head) { 
+		insertion->next = insertion; 
+		return insertion; 
+	} 
+	
+	if (head == head->next) {
+		head->next = insertion; 
+		insertion->next = head;
+		return insertion;
+	} 
+	
+	std::swap(head->data, insertion->data); 
+	insertion->next = head->next; 
+	head->next = insertion; 
+	return head;
+}
+
